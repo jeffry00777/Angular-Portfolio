2dcc6a8bebf7b3478809a7c27d8e87b1695a3571
Jeffry Livingston <50872197+jeffry00777@users.noreply.github.com>
2025-06-24 15:21:59 -0400

    Adding Projects

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,25 @@
-import { Component, ViewChild, ElementRef} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NgIf, NgFor  } from '@angular/common'; 
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NgIf, NgFor } from "@angular/common";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [RouterOutlet, NgFor, NgIf],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
 export class HomeComponent {
-
-  title = 'angular_2025';
-  text1 = "Chasing ";
-  text2 = "the American Dream";
+  title = "angular_2025";
+  text1 = "Hey there! I’m Jeffry";
+  text2 = "Turning Ideas into High-Performance Applications";
   typingSpeed = 100;
-  @ViewChild('videoPlayer', { static: false }) videoPlayer!: ElementRef;
+  @ViewChild("videoPlayer", { static: false }) videoPlayer!: ElementRef;
 
   videos = [
-    { src: 'assets/play.mp4' },
-    { src: 'assets/play.mp4' },
-    { src: 'assets/play.mp4' }
+    { src: "assets/play.mp4" },
+    { src: "assets/play.mp4" },
+    { src: "assets/play.mp4" },
   ];
 
   currentVideoIndex = 0;
@@ -31,7 +30,7 @@ export class HomeComponent {
     this.typeText("text-line-1", this.text1, 0, () => {
       setTimeout(() => {
         this.typeText("text-line-2", this.text2, 0);
-      }, 500); // Delay before second line starts typing
+      }, 500);
     });
   }
 
@@ -45,26 +44,32 @@ export class HomeComponent {
       this.isPaused = true;
     }
   }
-  
 
   setVideo(index: number) {
     this.currentVideoIndex = index;
     this.updateVideoSource();
   }
-  
+
   updateVideoSource() {
     const video: HTMLVideoElement = this.videoPlayer.nativeElement;
     video.src = this.videos[this.currentVideoIndex].src;
     video.load();
     video.play();
   }
-  typeText(elementId: string, text: string, index: number, callback?: () => void) {
+  typeText(
+    elementId: string,
+    text: string,
+    index: number,
+    callback?: () => void
+  ) {
     if (index < text.length) {
       document.getElementById(elementId)!.innerHTML += text.charAt(index);
-      setTimeout(() => this.typeText(elementId, text, index + 1, callback), this.typingSpeed);
+      setTimeout(
+        () => this.typeText(elementId, text, index + 1, callback),
+        this.typingSpeed
+      );
     } else if (callback) {
       callback();
     }
   }
-
 }
